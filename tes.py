9db1fd3cd8a7ba1fb00918b4de288cd20485747c
zaxkkcuyy-cpu <zaxkkcuyy@gmail.com>
2025-11-03 14:34:19 +0700

    Add basic Python program for user interaction

---
+++ tes.py
@@ -0,0 +1,21 @@
+print("TUGAS KKA BELJAR MENGCODING")
+
+materi = "belajar tentang Python"
+nama = "kesya"
+kelas = 10
+umur = 16
+
+print(materi, nama, kelas, umur)
+
+
+nama = input("Masukan nama kamu :")
+umur = int(input("Masukan umur kamuu :"))
+print(f"Halo nama saya {nama} dan umur saya {umur} tahun")
+
+nilai = int(input("Masukan nilai anda :"))
+if nilai >= 90:
+    print("NILAI KAMU A 😍😍😍😍😍")
+elif nilai >= 80:
+    print("NILAI KAMU B 😉😉😉")
+else:
+    print("yahh nilai kamu c nihhh gapapa tetp semangat yaaa😁😁😁")
